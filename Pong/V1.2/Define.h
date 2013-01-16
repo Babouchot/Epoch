c33689ac37d775b2f6c6552505854e463e92d17e
supermomo <maugard.matthieu@gmail.com>
2013-01-16 13:10:11 +0100

    Modification des fichiers

--- Pong/V1.2/Define.h
+++ Pong/V1.2/Define.h
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <fstream>
 
 
-#ifdef __APPLE__&__MACH__
+#ifdef __APPLE__&__MACH
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
 #else
