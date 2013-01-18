b7ca77d16f5f6d2a8fadb5948a92608355c84b32
Babouchot <bastien.maureille@hotmail.fr>
2013-01-18 17:41:18 +0100

    Deleted proprietary libs

--- Pong/V1.2/Jeu.h
+++ Pong/V1.2/Jeu.h
@@ -25,7 +25,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#ifdef __APPLE__&__MACH
+#ifdef __APPLE__MACH
 #include <OpenGL/glut.h>
 #else
 #include <GL/glut.h>
