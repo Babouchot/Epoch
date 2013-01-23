3862e72b3f679ce43866f575ffdfac3fda0cffbb
Babouchot <bastien.maureille@hotmail.fr>
2013-01-23 17:31:58 +0100

    fixing weird bug

--- Brain_FrameWork/Epoch_GUI/mainwindow.cpp
+++ Brain_FrameWork/Epoch_GUI/mainwindow.cpp
@@ -347,8 +347,10 @@ void MainWindow::on_addGameButton_clicked()
 {
    QString openFile = QFileDialog::getOpenFileName(this, tr("Select your game"),tr("/"));
    std::string file = openFile.toStdString();
-   games.push_back(file);
-   ui->gameComboBox->addItem(basename(file.c_str()));
+   if (file.size() > 0) {
+       games.push_back(file);
+       ui->gameComboBox->addItem(basename(file.c_str()));
+   }
 }
 
 void MainWindow::on_StartGameButton_clicked()
